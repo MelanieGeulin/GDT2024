2a0d8560745119a968aaee844b43fdbceb39062d
NikoMyoji <nicolas.vlte@gmail.com>
2024-09-27 14:03:19 +0200

    Last polish

--- Assets/Scripts/Controller/CueController.cs
+++ Assets/Scripts/Controller/CueController.cs
@@ -25,34 +25,16 @@ public class CueController : MonoBehaviour
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
-
-       
-        transform.rotation *= Quaternion.Euler(0f, xInput * _rotationSpeed * Time.deltaTime, 0f);
+        transform.rotation *= Quaternion.Euler(0f, -xInput * _rotationSpeed * Time.deltaTime, 0f);
 
 
-        if (Input.GetKeyDown(KeyCode.J))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Hit");
             _animator.SetTrigger("Hit");
         }
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            Rigidbody rb = _ball.GetComponent<Rigidbody>();
-            if (rb == null)
-            {
-                Debug.LogError($"Missing rigidbody on {_ball.name}");
-            }
-            
-            rb.AddForce(Vector3.forward * 10f, ForceMode.Impulse);
-        }
-    }
-    public void PrintEvent()
-    {
-        Debug.Log("PrintEvent");
     }
     public void HitBall()
     {
-        Debug.Log("PrintEvent");
         Vector3 direction = transform.position - _cue.transform.position;
 
         Rigidbody rb = _ball.GetComponent<Rigidbody>();
